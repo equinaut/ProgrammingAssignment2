9e72de4f348fc2ae0a9cfaf357e2d69430c949f6
equinaut <matthewamos@live.com>
2015-09-24 23:07:19 -0400

    Final version
    Ready for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caches both a matrix and its inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Caches a matrix 'x' and, when called, stores its inverse value
+makeCacheMatrix <- function(x = matrix()) 
+{
+  minverse <- NULL
+  setmatrix = function(y) 
+  {
+    x <<- y
+    minverse <<- NULL
+  }
+  setinverse = function(z)
+  {
+    minverse <<- z
+  }
+  getmatrix = function() x
+  getinverse = function() minverse
+  
+  list(setmatrix = setmatrix, setinverse = setinverse,
+       getmatrix = getmatrix, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Returns a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  m <- x$getinverse()
+  if(!is.null(m))
+  {
+    message("getting cashed data")
+    return(m)
+  }
+  data <- x$getmatrix()
+  minverse <- solve(data)
+  x$setinverse(minverse)
+  
+  minverse
 }
